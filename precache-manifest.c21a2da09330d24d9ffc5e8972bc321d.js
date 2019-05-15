592f026e062ce6f1a30e36a88e34f5c8555e2a31
classlfz <classlfz@qq.com>
2019-05-15 16:22:52 +0800

    Updates

---
+++ precache-manifest.c21a2da09330d24d9ffc5e8972bc321d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225fd0bc095ca81e2d5e97a8b53b618d",
+    "url": "/gomoku-react/index.html"
+  },
+  {
+    "revision": "6ff2eae792ac12f1dd53",
+    "url": "/gomoku-react/static/css/main.4d39c55c.chunk.css"
+  },
+  {
+    "revision": "afa64caa32d2fa731975",
+    "url": "/gomoku-react/static/js/2.bc3af191.chunk.js"
+  },
+  {
+    "revision": "6ff2eae792ac12f1dd53",
+    "url": "/gomoku-react/static/js/main.5369ca6a.chunk.js"
+  },
+  {
+    "revision": "d2716ead9a1d606ac089",
+    "url": "/gomoku-react/static/js/runtime~main.16c63df4.js"
+  }
+]);
